4bcdb76a8877b77c7a0826836738a05adb9fb234
Davlatshoh <davlatshoh6275@gmail.com>
2023-10-25 18:24:39 +0300

    created more things :))

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,8 @@ function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
-
         <div
-            className="
+          className="
                 absolute
                 top-0
                 left-0
@@ -60,7 +59,7 @@ function Header() {
       </div>
 
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
-        <p className="flex items-center p-5  text-sm font-light pr-5  shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1] " >
+        <p className="flex items-center p-5  text-sm font-light pr-5  shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1] ">
           <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1 " />
           Lorem ipsum dolor sit amet.
         </p>
